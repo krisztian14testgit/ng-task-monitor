12040c7039161b1941e509659279d86c3e8a4951
krisztian14testgit <krisztian14@citromail.hu>
2022-01-29 23:12:24 +0100

    created basic route navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{path: 'inprogress', component: undefined},
+  {path: 'finished', component: undefined},*/
+  // opening side:
+  {path: '', redirectTo: '/', pathMatch: 'full'}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-task-monitor';
+export class AppComponent implements OnInit {
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {
+    console.log('router=', this.router.url);
+  }
 }
