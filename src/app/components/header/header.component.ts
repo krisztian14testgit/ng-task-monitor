5c27c6cd80d88253eb714993b54ff1057de1f53b
krisztian14testgit <krisztian14@citromail.hu>
2022-01-29 21:30:25 +0100

    menu header gid setting up

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,8 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  /** Contains the structure of the menu with labels and sub-menu items. */
+  public menusItemList_dict: {[label: string]: { key: string, name: string}[] };
+  public optionItemList_dict: {[label: string]: { key: string, name: string}[] };
 
-  constructor() { }
+  constructor() {
+    this.menusItemList_dict = {
+      Tasks: [
+        {key: "inprogress", name: "In-Progress"},
+        {key: "finished", name: "Finished"}
+      ],
+      Charts: [
+        {key: "weekly", name: "In-Weekly"},
+        {key: "statistic", name: "Statistic-all"}
+      ]
+    }
+
+    this.optionItemList_dict = {
+      Location: [
+        {key: "location", name: "Change location"}
+      ]
+    }
+  }
 
   ngOnInit(): void {
   }
