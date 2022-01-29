b0b572d2e5dd8f0400e980c8dec4f9b6220aa203
krisztian14testgit <krisztian14@citromail.hu>
2022-01-29 19:49:42 +0100

    menu-item displaying is done

--- src/app/components/menu-item/menu-item.component.ts
+++ src/app/components/menu-item/menu-item.component.ts
@@ -1,21 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 /**
- * Displays the nested menu.
+ * Displays the menu with nested menus.
  */
 @Component({
   selector: 'app-menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.css']
 })
-export class MenuItemComponent implements OnInit {
+export class MenuItemComponent implements OnChanges {
   @Input() public title!: string;
-  @Input() public subMenuItems_dict!: {[key: string]: string};
+  @Input() public subMenuItems_dict!: {[topic: string]: { key: string, name: string}[] };
   @Input() public isDisplayedKeys = true;
+
+  public subMenuValues: {key: string, name: string}[] = [];
+  public subMenuKeys: string[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  /**
+   * If this.isDisplayKeys is false, it collects the list items from dictionary without topic key.
+   * Showing the submenus item without topic key.
+   */
+  ngOnChanges(): void {
+    if (this.subMenuItems_dict) {
+      this.subMenuKeys = Object.keys(this.subMenuItems_dict);
+    }
+
+    if (!this.isDisplayedKeys) {
+      // get sub-array into one array: [ [a,b], [c,d] ] => [a,b,c,d]
+      Object.values(this.subMenuItems_dict).forEach(getList => this.subMenuValues = [...this.subMenuValues, ...getList]);
+    }
   }
 
 }
