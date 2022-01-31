38f24b5115622a4578bc771a866309c047e8042f
krisztian14testgit <krisztian14@citromail.hu>
2022-01-31 14:34:19 +0100

    chage-location.comp attached in lazy-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   /*{path: 'inprogress', component: undefined},
   {path: 'finished', component: undefined},*/
+  // lazy loading part:
+  {
+    path: 'location',
+    loadChildren: () => import('./modules/change-location/change-location.module').then(m => m.ChangeLocationModule)
+  },
   // opening side:
-  {path: '', redirectTo: '/', pathMatch: 'full'}, 
+  {path: '', redirectTo: '/', pathMatch: 'full'},
 ];
 
 @NgModule({
--- src/app/modules/change-location/change-location-routing.module.ts
+++ src/app/modules/change-location/change-location-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChangeLocationComponent } from './change-location.component';
+
+const routes: Routes = [
+  {path: '', component: ChangeLocationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
