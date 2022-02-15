4eba7cb5327f08cd1af8a98fdd247ec3e3750147
krisztian14testgit <krisztian14@citromail.hu>
2022-02-15 21:46:27 +0100

    request locatoin service testing is done

--- src/app/modules/change-location/services/location/location-service.spec.ts
+++ src/app/modules/change-location/services/location/location-service.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { LocationService } from './location-service';
+import { LocationPath, LocationService } from './location-service';
+import { LocationSetting } from './location-setting';
 
 /**
  * Help writing http test
@@ -8,16 +9,64 @@ import { LocationService } from './location-service';
  */
 describe('LocationService', () => {
   let service: LocationService;
+  let mockHttp: HttpTestingController;
+  const locationUrl = 'localhost:8080/location';
+  const fakedPath = 'C:/Users/../Documents/';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [LocationService]
     });
+
     service = TestBed.inject(LocationService);
+    mockHttp = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be created', inject([LocationService], (instanceObj: LocationService ) => {
+    expect(instanceObj).toBeTruthy();
+  }));
+
+  it('should get location setting', () => {
+    service.getLocationSetting()
+    .subscribe((locSetting: LocationSetting) => {
+      expect(locSetting).toBeDefined();
+      expect(locSetting.appSettingPath).toBe(fakedPath);
+      expect(locSetting.taskPath).toBe(fakedPath);
+    });
+
+    const req = mockHttp.expectOne({url: locationUrl, method: 'GET'});
+    expect(req.request.method).toBe('GET');
+
+    // sent dummy data
+    const dummyData = new LocationSetting();
+    dummyData.appSettingPath = fakedPath;
+    dummyData.taskPath = fakedPath;
+    req.flush(dummyData);
+  });
+
+  it('should save location setting', () => {
+    service.saveLocation(LocationPath.AppSettingPath, fakedPath)
+    .subscribe(isTrue => expect(isTrue).toBeTrue());
+
+    const req = mockHttp.expectOne({url: locationUrl, method: 'POST'});
+    expect(req.request.method).toBe('POST');
+
+    // sent dummy data
+    req.flush(true); // saving was success
+  });
+
+  it('should save location setting is failed, 503', () => {
+    service.saveLocation(LocationPath.AppSettingPath, fakedPath)
+    .subscribe(_ => {}, (err) =>  {
+      expect(err.status).toBe(503);
+      expect(err.statusText).toBe('Bad request');
+    });
+
+    const req = mockHttp.expectOne({url: locationUrl, method: 'POST'});
+    // expect(req).toBe('POST');
+
+    // sent dummy data
+    req.flush(false, {status: 503, statusText: 'Bad request'});
   });
 });
--- src/app/modules/change-location/services/location/location-service.ts
+++ src/app/modules/change-location/services/location/location-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, from, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { LocationSetting } from './location-setting';
