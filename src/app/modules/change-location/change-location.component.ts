163ee3eb0dc5afe36953b1325f020bcce62e0721
krisztian14testgit <krisztian14@citromail.hu>
2022-02-16 18:59:05 +0100

    path saving is done, by keyup event

--- src/app/modules/change-location/change-location.component.ts
+++ src/app/modules/change-location/change-location.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+
 import { MyValidator } from 'src/app/validators/my-validator';
+import { LocationPath, LocationService } from './services/location/location-service';
+import { LocationSetting } from './services/location/location-setting';
 
 @Component({
   selector: 'app-change-location',
@@ -21,29 +25,54 @@ export class ChangeLocationComponent implements OnInit {
   public get appSettingControl(): FormControl {
     return this._locationForm.get('appSettingPath') as FormControl;
   }
+  //#endregion
 
-  constructor() { }
+  constructor(private readonly locationService: LocationService) {
+    this.createLocationFormValidation();
+  }
 
-  /** Initialization the locationFrom instance from the FormGroup. */
+  /** Set the location paths by the location service which come from server. */
   ngOnInit(): void {
-    this._locationForm = new FormGroup({
-      taskDataPath: new FormControl('', [
-        Validators.required, 
-        Validators.pattern(MyValidator.Patterns.getRule(MyValidator.PatternRuleKeys.LibraryPath))
-      ]),
-      appSettingPath: new FormControl('alma', [
-        Validators.required,
-        Validators.pattern(MyValidator.Patterns.getRule(MyValidator.PatternRuleKeys.LibraryPath))
-      ])
+    this.locationService.getLocationSetting()
+    .subscribe((locSetting: LocationSetting) => {
+      this.appSettingControl.setValue(locSetting.appSettingPath);
+      this.taskDataControl.setValue(locSetting.taskPath);
     });
   }
 
   /**
    * This is an key-up event function.
+   * It runs when the typing is occured.
+   * Saving the tyoed folder path if it is valid.
+   *  
    * @event keyup
+   * @param keyLocation is string, value can be LocationPath
+   * @param formControlRef the reference of the given formControl.
    */
-  public onChangePath(controlValue: string): void {
-    console.log('taskPath=', controlValue);
+  public onChangePath(keyLocation: string, formControlRef: FormControl): void {
+    const waitSeconds = 2000;
+    // converting string to enum value
+    if (formControlRef.valid) {
+      const locKey = LocationPath[keyLocation as keyof typeof LocationPath];
+      this.locationService.saveLocation(locKey, formControlRef.value)
+      .pipe(debounceTime(waitSeconds))
+      .subscribe();
+    }
+    
+  }
+
+  /** Initialization the locationFrom instance from the FormGroup. */
+  private createLocationFormValidation(): void {
+    this._locationForm = new FormGroup({
+      taskDataPath: new FormControl('', [
+        Validators.required, 
+        Validators.pattern(MyValidator.Patterns.getRule(MyValidator.PatternRuleKeys.LibraryPath))
+      ]),
+      appSettingPath: new FormControl('', [
+        Validators.required,
+        Validators.pattern(MyValidator.Patterns.getRule(MyValidator.PatternRuleKeys.LibraryPath))
+      ])
+    });
   }
 
 }
