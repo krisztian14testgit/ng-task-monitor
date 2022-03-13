19295eb7f7ff2ec38559c4a73eda8ca9683e5701
krisztian14testgit <krisztian14@citromail.hu>
2022-03-13 14:01:26 +0100

    LocationPath enum moved into loc-setting.model

--- src/app/modules/change-location/services/location/location-setting.model.ts
+++ src/app/modules/change-location/services/location/location-setting.model.ts
@@ -1,4 +1,17 @@
+/**
+ * Represents the those path property which are available.
+ * It helps for the location.service to store the diff path into the diff properties.
+ */
 export class LocationSetting {
     appSettingPath!: string;
     taskPath!: string;
+}
+
+/**
+ * Adjustable paths of the Location stuctucre.
+ * It can help for setting the pathType
+ */
+export enum LocationPath {
+    AppSettingPath,
+    TaskPath
 }
