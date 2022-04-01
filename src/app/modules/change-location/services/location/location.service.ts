2f4bb1e0a58dec0f73426d52ac1324eb8cc2596f
krisztian14testgit <krisztian14@citromail.hu>
2022-04-01 20:38:26 +0200

    locationUrl is linked from environment.host var

--- src/app/modules/change-location/services/location/location.service.ts
+++ src/app/modules/change-location/services/location/location.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { environment } from 'src/environments/environment';
 import { LocationPath, LocationSetting } from './location-setting.model';
 import ServiceBase from 'src/app/services/service-base';
 
@@ -11,7 +12,7 @@ export class LocationService {
 
   private readonly _defaultPath: string;
   private readonly _locSetting!: LocationSetting | {[prop: string]: string};
-  private readonly _locationUrl = 'http://localhost:8080/location';
+  private readonly _locationUrl = `${environment.host}location`;
 
   constructor(private readonly http: HttpClient) {
     this._defaultPath = 'C:/Users/../Documents/';
