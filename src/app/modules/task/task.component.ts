ef8e227c4feefb77287cc95016c63490b2ee650c
krisztian14testgit <krisztian14@citromail.hu>
2022-04-16 16:48:44 +0200

    taks statuses filled in selection

--- src/app/modules/task/task.component.ts
+++ src/app/modules/task/task.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 import { TaskService } from './services/task.service';
-import { Task } from './services/task.model';
+import { Task, TaskStatus } from './services/task.model';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -9,8 +9,11 @@ import { Subscription } from 'rxjs';
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent implements OnInit, OnDestroy {
+export class TaskComponent implements OnInit, AfterViewInit, OnDestroy {
+  /** Stores the created tasks. */
   public taskList: Task[] = [];
+  /** Statuses of the Task. */
+  public taksStatusList: string[] = [];
   private _taskSubscription!: Subscription;
 
   constructor(private readonly taskService: TaskService) { }
@@ -19,12 +22,27 @@ export class TaskComponent implements OnInit, OnDestroy {
     this.getAllTask();
   }
 
+  ngAfterViewInit(): void {
+    this.fillInStatusSelection();
+  }
+
   ngOnDestroy(): void {
     this._taskSubscription.unsubscribe();
   }
 
   private getAllTask(): void {
     this._taskSubscription = this.taskService.getAll().subscribe(tasks => this.taskList = tasks);
+  }
+
+  private fillInStatusSelection(): void {
+    const statuses = Object.values(TaskStatus) as string[];
+    // get rid of number: "0", "1", 
+    const notSingleNumber = 1;
+    const filteredStatuses = statuses.filter((item: string) => item.length > notSingleNumber);
+    
+    for (const status of filteredStatuses) {
+      this.taksStatusList.push(status.toLowerCase());
+    }
   }
 
 }
