30530dcb377ef60fc1cc592499082897c04d8dee
krisztian14testgit <krisztian14@citromail.hu>
2022-04-17 11:10:10 +0200

    task service using FakedTask, temporary for test

--- src/app/modules/task/services/task.service.ts
+++ src/app/modules/task/services/task.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import ServiceBase from 'src/app/services/service-base';
 import { Task } from './task.model';
 import { environment } from '../../../../environments/environment';
+
+import { FakedTask } from '../../../tests/models/faked-task.model';
 
 @Injectable()
 export class TaskService {
@@ -23,12 +25,16 @@ export class TaskService {
    * @returns Task[]
    */
   public getAll(): Observable<Task[]> {
-    return this.http.get<Task[]>(this._taskUrl, {headers: ServiceBase.HttpHeaders})
+    
+    /*return this.http.get<Task[]>(this._taskUrl, {headers: ServiceBase.HttpHeaders})
     .pipe(map((tasks: Task[]) => {
       this._taskList = tasks;
       this.taskList$.next(tasks);
       return tasks;
-    }));
+    }));*/
+
+    this.taskList$.next(FakedTask.list);
+    return of(FakedTask.list);
   }
 
   /**
@@ -37,7 +43,8 @@ export class TaskService {
    * @returns Task
    */
   public get(taskId: string): Observable<Task> {
-    return this.http.get<Task>(`${this._taskUrl}/${taskId}`, {headers: ServiceBase.HttpHeaders});
+    // return this.http.get<Task>(`${this._taskUrl}/${taskId}`, {headers: ServiceBase.HttpHeaders});
+    return of(FakedTask.list.find(task => task.id === taskId) as Task);
   }
 
   /**
@@ -46,12 +53,16 @@ export class TaskService {
    * @returns new Task
    */
   public add(task: Task): Observable<Task> {
-    return this.http.post<Task>(`${this._taskUrl}/${task.id}`, task, {headers: ServiceBase.HttpHeaders})
+    /*return this.http.post<Task>(`${this._taskUrl}/`, task, {headers: ServiceBase.HttpHeaders})
     .pipe(map((newTask: Task) => {
       this._taskList.push(newTask);
       this.taskList$.next(this._taskList);
       return newTask;
-    }));
+    }));*/
+
+    FakedTask.addNewTask(task);
+    this.taskList$.next(FakedTask.list);
+    return of(FakedTask.getLatestNewTask());
   }
 
   /**
@@ -60,14 +71,19 @@ export class TaskService {
    * @returns updated Task
    */
   public update(task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this._taskUrl}/${task.id}`, task, {headers: ServiceBase.HttpHeaders})
+    /*return this.http.put<Task>(`${this._taskUrl}/${task.id}`, task, {headers: ServiceBase.HttpHeaders})
     .pipe(map((updatedTask: Task) => {
       // The checking is unnecessary as the request run into good branch, so server found the item in the list and updated it.
       const foundTaskIndex = this._taskList.findIndex(taskItem => taskItem.id === updatedTask.id);
       this._taskList[foundTaskIndex] = updatedTask;
       this.taskList$.next(this._taskList);
       return updatedTask;
-    }));
+    }));*/
+
+    const foundTaskIndex = FakedTask.list.findIndex(taskItem => taskItem.id === task.id);
+    this._taskList[foundTaskIndex] = task;
+    this.taskList$.next(FakedTask.list);
+    return of(task);
   }
 
   /**
@@ -77,11 +93,17 @@ export class TaskService {
    * @returns boolean
    */
   public delete(taskId: string): Observable<boolean> {
-    return this.http.delete(`${this._taskUrl}/${taskId}`, {headers: ServiceBase.HttpHeaders})
+    /*return this.http.delete(`${this._taskUrl}/${taskId}`, {headers: ServiceBase.HttpHeaders})
     .pipe(map(_ => {
       const taskIndex = this._taskList.findIndex(task => task.id === taskId);
       this._taskList.splice(taskIndex, 1);
+      this.taskList$.next(this._taskList);
       return true;
-    }));
+    }));*/
+
+    const fakedIndex = FakedTask.list.findIndex(task => task.id === taskId);
+    this._taskList.splice(fakedIndex, 1);
+    this.taskList$.next(this._taskList);
+    return of(true);
   }
 }
