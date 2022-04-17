98a5c4ab6b850587c64ed9161711a426b35283c0
krisztian14testgit <krisztian14@citromail.hu>
2022-04-17 12:39:58 +0200

    task-card shows task props

--- src/app/modules/task/task-card/task-card/task-card.component.ts
+++ src/app/modules/task/task-card/task-card/task-card.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+
+import { Task, TaskStatus } from '../../services/task.model';
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.css']
 })
-export class TaskCardComponent implements OnInit {
+export class TaskCardComponent implements OnInit, OnChanges {
+  /** The current task reference which was given. */
+  @Input() public task: Task = new Task();
+
+  /** The name of TaskStatus. */
+  public statusLabel = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    if (this.task.id) {
+      // get TaskStatus key as string
+      this.statusLabel = TaskStatus[this.task.status];
+    }
   }
 
 }
