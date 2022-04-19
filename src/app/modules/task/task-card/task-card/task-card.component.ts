9e671c2831d14873210d17a47f0015a3daefb368
krisztian14testgit <krisztian14@citromail.hu>
2022-04-19 17:52:58 +0200

    task validation, saving task

--- src/app/modules/task/task-card/task-card/task-card.component.ts
+++ src/app/modules/task/task-card/task-card/task-card.component.ts
@@ -1,7 +1,10 @@
-import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertMessageService } from 'src/app/services/alert-message/alert-message.service';
+import { MyValidator } from 'src/app/validators/my-validator';
 
 import { Task, TaskStatus } from '../../services/task.model';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-card',
@@ -18,11 +21,16 @@ export class TaskCardComponent implements OnInit, OnChanges, AfterViewInit {
   public selectedTaskId = '';
   /** True: The card is selected, otherwise false. */
   public isSelected = false;
-  public taskControls: {[prop: string]: FormControl};
+  /** 
+   * Stores the references of the formControls of the taks reactive form.
+   * It is helping contruction to get current formControl form the Formgroup.
+   */
+  public taskControls: {[prop: string]: FormControl };
+  /** The FromGroup structure of the task. */
+  public taskForm!: FormGroup;
 
-  private _taskForm!: FormGroup;
-
-  constructor() { 
+  constructor(private readonly taskService: TaskService,
+              private readonly alertMessageService: AlertMessageService) { 
     this.taskControls = {
       title: new FormControl(),
       description: new FormControl(),
@@ -37,35 +45,71 @@ export class TaskCardComponent implements OnInit, OnChanges, AfterViewInit {
     if (this.task.id) {
       // get TaskStatus key as string
       this.statusLabel = TaskStatus[this.task.status];
+      this.taskForm = this.generateReactiveForm(this.task);
     }
   }
 
   ngAfterViewInit(): void {
     if (this.isEditable) {
-      this._taskForm = this.generateReactiveForm(this.task);
-      this.taskControls['title'] = this._taskForm.get('title') as FormControl;
-      this.taskControls['description'] = this._taskForm.get('description') as FormControl;
-      this.taskControls['timeSeconds'] = this._taskForm.get('timeSeconds') as FormControl;
+      this.taskControls['title'] = this.taskForm.get('title') as FormControl;
+      this.taskControls['description'] = this.taskForm.get('description') as FormControl;
+      this.taskControls['timeSeconds'] = this.taskForm.get('timeSeconds') as FormControl;
     }
   }
 
+  /**
+   * This is an click event function.
+   * It runs when the user click on the card.
+   */
   public onClickMatCard(): void {
     this.selectedTaskId = this.task.id;
     this.isSelected = true;
   }
 
+  /**
+   * This an event function.
+   * It runs when the save button is triggered.
+   * Saving/updating the task by the taskService.
+   */
+  public onSaveCard(): void {
+    const isNewTask = this.task.id.length > 0;
+    const errorText = 'Updating/saving has been unsuccessful, server error!';
+    // task instance contains the updated value by the taksForm by the references!
+    this.taskForm.updateValueAndValidity();
+    
+    if (!isNewTask) {
+      // update the existed task
+      this.taskService.update(this.task)
+      .subscribe(updatedTask => this.task = updatedTask,
+        _ => this.alertMessageService.sendMessage(errorText));
+    } else {
+      // insert new task
+      this.taskService.add(this.task)
+      .subscribe(insertedTask => this.task = insertedTask,
+        _ => this.alertMessageService.sendMessage(errorText));
+    }
+   
+  }
+
+  /**
+   * Returns the Reactive form from the given task instance.
+   * @param task The instance of the given task.
+   * @returns FormGroup
+   */
   private generateReactiveForm(task: Task): FormGroup {
     return new FormGroup({
       title: new FormControl(task.title, [
-        Validators.required
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+        Validators.pattern(MyValidator.Patterns.getRule(MyValidator.PatternRuleKeys.TaskName))
       ]),
-      description: new FormControl(task.description, [
-        Validators.required
-      ]),
+      description: new FormControl(task.description, []),
       timeSeconds: new FormControl(task.timeSeconds, [
-        Validators.required
+        Validators.required,
+        Validators.max(24 * 60), // 24h => 24 * 60min
+        Validators.min(1) //min value: 1 min
       ])
     });
   }
-
 }
