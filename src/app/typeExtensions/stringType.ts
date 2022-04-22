f678200960c540a8e7ce4d5c7455585bbb4a7ef3
krisztian14testgit <krisztian14@citromail.hu>
2022-04-22 20:50:16 +0200

    toUpperCaseFirstChar() is created for String via prototype

---
+++ src/app/typeExtensions/stringType.ts
@@ -0,0 +1,21 @@
+/**
+ * Helping the extend primitve types with your functions:
+ * https://codepunk.io/extending-strings-and-other-data-types-in-typescript/
+ */
+
+interface String {
+    /** Converts the first character to upper case. */
+    toUpperCaseFirstChar: () => string;
+}
+
+String.prototype.toUpperCaseFirstChar = function(): string {
+    if (this.length > 0) {
+        let firstChar = this[0];
+        firstChar = firstChar.toUpperCase();
+        return firstChar + this.substring(1, this.length);
+    }
+    
+    return '';
+};
+
+
