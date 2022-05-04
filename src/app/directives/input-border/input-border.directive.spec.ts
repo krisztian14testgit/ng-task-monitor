33861777abcfd725c953deec3d80e4cc2d36fb83
krisztian14testgit <krisztian14@citromail.hu>
2022-05-04 21:59:11 +0200

    input-border unitTests done

---
+++ src/app/directives/input-border/input-border.directive.spec.ts
@@ -0,0 +1,124 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { InputBorderDirective } from './input-border.directive';
+
+@Component({
+  template: `
+    <input dirInputBorder [isValid]="false"
+      id="test-input" type="text" class="input-control"/>
+  `
+})
+class HostTestComponent {}
+
+describe('InputBorderDirective', () => {
+  let fixture: ComponentFixture<HostTestComponent>;
+  let directive: InputBorderDirective;
+  let inputTag: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [InputBorderDirective, HostTestComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HostTestComponent);
+    fixture.detectChanges();
+
+    // get directive instance via debugElement
+    inputTag = fixture.debugElement.query(By.css('#test-input'));
+    directive = inputTag.injector.get(InputBorderDirective);
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should find the class by findKey', () => {
+    // when it is empty
+    let classes: string[] = [];
+    let findKey = 'myClass';
+    let expectedIndex = -1;
+    
+    let foundIndex = directive['findCurrentClassBy'](classes, findKey);
+    expect(foundIndex).toBe(expectedIndex);
+
+    // when classes is not empty, not find class by findKey
+    classes = ['input-control', 'other-class'];
+    foundIndex = directive['findCurrentClassBy'](classes, findKey);
+    expect(foundIndex).toBe(expectedIndex);
+
+    // when find is empty, not find class
+    findKey = '';
+    foundIndex = directive['findCurrentClassBy'](classes, findKey);
+    expect(foundIndex).toBe(expectedIndex);
+
+    // when class found class
+    classes = ['input-control', 'other-class', 'myClass'];
+    findKey = 'myClass';
+    expectedIndex = 2;
+    foundIndex = directive['findCurrentClassBy'](classes, findKey);
+    expect(foundIndex).toBe(expectedIndex);
+
+    // when get found class, classes order is changed.
+    classes = ['input-control', 'myClass', 'other-class'];
+    expectedIndex = 1;
+    foundIndex = directive['findCurrentClassBy'](classes, findKey);
+    expect(foundIndex).toBe(expectedIndex);
+  });
+
+  it('should add/remove the input-valid or input-invalid class from inputTag', () => {
+    // add input-valid class
+    let className = 'input-valid';
+    inputTag.nativeElement.className += ' ' + className;
+    directive['refInput'] = inputTag.nativeElement;
+    expect(directive['refInput'].className.includes(className)).toBeTrue();
+
+    // remove input-valid class by clearPreviousBorderClass
+    directive['clearPreviousBorderClass']();
+    expect(directive['refInput'].className.includes(className)).toBeFalse();
+
+    // add input-invalid class
+    className = 'input-invalid';
+    inputTag.nativeElement.className += ' ' + className;
+    directive['refInput'] = inputTag.nativeElement;
+    expect(directive['refInput'].className.includes(className)).toBeTrue();
+
+    // remove input-invalid class by clearPreviousBorderClass
+    directive['clearPreviousBorderClass']();
+    expect(directive['refInput'].className.includes(className)).toBeFalse();
+
+    // add random class with 'input' keyword
+    className = 'input-random';
+    inputTag.nativeElement.className += ' ' + className;
+    directive['refInput'] = inputTag.nativeElement;
+    expect(directive['refInput'].className.includes(className)).toBeTrue();
+
+    // remove random class by clearPreviousBorderClass
+    directive['clearPreviousBorderClass']();
+    expect(directive['refInput'].className.includes(className)).toBeFalse();
+  });
+
+  it('should not call clearPreviousBorderClass, if input className is empy!', () => {
+    spyOn(directive as any, 'findCurrentClassBy').and.callThrough();
+    
+    inputTag.nativeElement.className = '';
+    directive['refInput'] = inputTag.nativeElement;
+    directive['clearPreviousBorderClass']();
+    expect(directive['findCurrentClassBy']).not.toHaveBeenCalled();
+  });
+
+  it('should apply input classes(valid, invalid) base on isValid', () => {
+    directive['refInput'] = inputTag.nativeElement;
+    // apply the input-valid
+    directive.isValid = true;
+    // calls changeBorderBy method
+    directive.ngOnChanges();
+    expect(directive['refInput'].className.includes('input-valid')).toBeTrue();
+
+    // apply the input-invalid
+    directive.isValid = false;
+    // calls changeBorderBy method
+    directive.ngOnChanges();
+    expect(directive['refInput'].className.includes('input-invalid')).toBeTrue();
+  });
+});
