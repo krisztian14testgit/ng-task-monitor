ad8fac89634dc8a588cea76d4f0272761407dd0a
krisztian14testgit <krisztian14@citromail.hu>
2022-05-07 14:20:57 +0200

    header url navigation tests are done

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,19 +1,42 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { Location } from "@angular/common";
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { HeaderComponent } from './header.component';
+
+@Component({})
+class FakedRouteComponent {}
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
   let router: Router;
-  const routeTable: any[] = [];
+  let location: Location;
+  const routeTable: any[] =  [
+    {path: 'home', component: FakedRouteComponent },
+    {path: 'tasks/inprogress', component: FakedRouteComponent },
+    {path: 'weekly', component: FakedRouteComponent },
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+  ];
+
+  /** merged menu items from appMenu & optionMenu */
+  const expectedRouterDict = {
+    "tasks/inprogress": "In-Progress",
+    "tasks/finished": "Finished",
+    "weekly": "In-Weekly",
+    "statistic": "Statistic-all",
+    "location": "Change location"
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes(routeTable)],
+      imports: [
+        RouterTestingModule.withRoutes(routeTable),
+        MatGridListModule
+      ],
       declarations: [ HeaderComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
@@ -24,10 +47,43 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+
+    router.initialNavigation();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.optionMenus).toBeDefined();
+    expect(component.appMenus).toBeDefined();
   });
+
+  it('should checked filled the rotuer dict after the component declaration', () => {
+    expect(component['routerDict']).toEqual(expectedRouterDict);
+  });
+
+  it('should navigate to faked home site', fakeAsync(()=> {
+    router.navigate(['']); // navige to tasks/inprogress
+    tick(1);
+    expect(location.path()).toBe('/home');
+  }));
+
+  it('should get titleOfRoute from the router event', fakeAsync(() => {
+    expect(component.titleOfRoute).toBe('');
+    // subscribe on the chaging router event by ngOnInit
+    component.ngOnInit();
+
+    // tasks/inprogress and weekly are defined in a routerTable
+    const navigationPaths = ['tasks/inprogress', 'weekly'];
+    for (const navUrl of navigationPaths) {
+      router.navigate([ navUrl ]);
+      tick(1);
+      expect(location.path()).toBe(`/${navUrl}`);
+      // routerDict contains the title of each router path.
+      expect(component.titleOfRoute).toBe(component['routerDict'][navUrl]);
+    }
+
+    flush();
+  }));
 });
