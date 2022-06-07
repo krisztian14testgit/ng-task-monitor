b87cf6e27298e8bf1d0094b60502115a775c4768
krisztian14testgit <krisztian14@citromail.hu>
2022-06-07 22:12:26 +0200

    convertsToMilliSec, converting minutesAndSec to milliSec otherway

--- src/app/modules/task/services/task-timer.model.ts
+++ src/app/modules/task/services/task-timer.model.ts
@@ -1,14 +1,35 @@
 /**
- * It is in charge of converting different task times to desired one.
+ * It is in charge of converting different times to desired one.
  */
- export class TaskTimer { // => moved into new model file: TaskTimer.model.ts
-    /** Returns the milliSeconds from the given minutes. */
-    static convertsToMilliSec(inMinutes: number): number {
-        if (inMinutes === 0) { return 0; }
+ export class TaskTimer {
+    /**
+     * Returns the milliSeconds from the given minutesAndSecond.
+     * @description
+     * At the decimal number:
+     * * Integer => minutes
+     * * Fraction => seconds
+     * * E.g: 3.45 => Minutues: 3, seconds: 45.
+     *
+     * @param minutesAndSec It can be integer egy decimal number.
+     * */
+    static convertsToMilliSec(minutesAndSec: number): number {
+        if (minutesAndSec === 0) { return 0; }
         
         const sec = 60;
         const milliSec = 1000;
-        return inMinutes * sec * milliSec;
+        // get fraction
+        if (minutesAndSec % 1 !== 0) {
+            // [mintues, seconds]
+            const stringNumArray: string[] = minutesAndSec.toString().split('.');
+            const minutesInSeconds = Number.parseInt(stringNumArray[0]) * 60;
+            let seconds =  Number.parseInt(stringNumArray[1]);
+            // 1.2 => 2 not sec, reather it will 20sec, increase the digit position: Tens
+            seconds = seconds < 10 ? seconds * 10: seconds;
+            return (minutesInSeconds + seconds) * milliSec;
+        }
+
+        // return interger number
+        return minutesAndSec * sec * milliSec;
     }
 }
 
