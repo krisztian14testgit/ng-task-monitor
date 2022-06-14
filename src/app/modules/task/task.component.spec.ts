9c12b99d699f1e7aefcb9678c4863a937f1136ef
krisztian14testgit <krisztian14@citromail.hu>
2022-06-14 12:07:35 +0200

    task comp includes providers for test cases

--- src/app/modules/task/task.component.spec.ts
+++ src/app/modules/task/task.component.spec.ts
@@ -1,4 +1,11 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlertMessageService } from 'src/app/services/alert-message/alert-message.service';
+import { CountdownTimerService } from 'src/app/services/countdown-timer/countdown-timer.service';
+import { MockCountdownTimerService } from 'src/app/tests/mock-services/mock-countdown-timer.service';
+import { MockTaskService } from 'src/app/tests/mock-services/mock-task.service';
+import { TaskService } from './services/task.service';
 
 import { TaskComponent } from './task.component';
 
@@ -8,7 +15,13 @@ describe('TaskComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskComponent ]
+      declarations: [ TaskComponent ],
+      providers: [
+        { provide: TaskService, useClass: MockTaskService },
+        { provide: CountdownTimerService, useClass: MockCountdownTimerService },
+        { provide: AlertMessageService }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   });
