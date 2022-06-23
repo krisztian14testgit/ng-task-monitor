b95eec9d3399f405c56fab09ece58673ce67e86a
krisztian14testgit <krisztian14@citromail.hu>
2022-06-23 18:30:41 +0200

    display the taskStatus by number of counts

--- src/app/modules/statistic/task-count/task-count.component.ts
+++ src/app/modules/statistic/task-count/task-count.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+
+import { Task, TaskStatus } from '../../task/services/task.model';
+
+enum ChartBackGroundColor {
+  Start = 'rgb(125, 125, 200)',
+  InProgress = 'rgb(255, 100, 0)',
+  Completed = 'rgb(0, 155, 0)'
+}
 
 @Component({
   selector: 'app-task-count',
   templateUrl: './task-count.component.html',
   styleUrls: ['./task-count.component.css']
 })
-export class TaskCountComponent implements OnInit {
+export class TaskCountComponent implements OnChanges {
+  @Input() taskList: Task[] = [];
+  @Input() isShowedTodayDate = false;
+
+  public filteredTaskList: Task[] = [];
 
   public pieChartType: ChartType;
   public pieChartOptions: ChartConfiguration['options'];
@@ -25,16 +37,63 @@ export class TaskCountComponent implements OnInit {
       }
     };
     this.pieChartData = {
-      labels: [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ],
+      labels: [],
       datasets: [ {
-        data: [ 300, 500, 100 ]
+        label: 'Counts of Task status',
+        data: [],
+        backgroundColor: [
+          ChartBackGroundColor.Start,
+          ChartBackGroundColor.InProgress,
+          ChartBackGroundColor.Completed
+        ],
+        hoverBackgroundColor: [
+          ChartBackGroundColor.Start,
+          ChartBackGroundColor.InProgress,
+          ChartBackGroundColor.Completed
+        ]
       } ]
     };
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    if (this.taskList.length > 0) {
+      this.filteredTaskList = this.taskList;
+      
+      if (this.isShowedTodayDate) {
+        this.filteredTaskList = this.filterByCreatedToday(this.taskList);
+      }
+      this.setPieChartDataBy(this.filteredTaskList);
+    }
   }
 
-  
+  private filterByCreatedToday(taskList: Task[]): Task[] {
+    return taskList.filter(task => task.isCreatedToday());
+  }
 
+  private setPieChartDataBy(taskList: Task[]): void {
+    // datasets reset
+    const taskStatusCounts = this.getCountOfTaskStatuses(taskList);
+    this.pieChartData.datasets[0].data = taskStatusCounts;
+    this.pieChartData.labels = this.getChartLabelsByTaskStatus(taskStatusCounts);
+  }
+
+  private getChartLabelsByTaskStatus(displayStatusCounts: number[]): string[] {
+    const notBeIndexNumber = 1;
+    const retLables = Object.keys(TaskStatus).filter(key => key.length > notBeIndexNumber);
+    for (let i = 0; i < retLables.length; i++) {
+      retLables[i] += `(${displayStatusCounts[i]})`;
+    }
+
+    return retLables;
+  }
+
+  private getCountOfTaskStatuses(taskList: Task[]): number[] {
+    // 0. pos => Start, 1. pos => inprogress, 2. pos => completed
+    const taskStatusCounters = [0, 0, 0];
+    for (const task of taskList) {
+      taskStatusCounters[task.status] += 1;
+    }
+
+    return taskStatusCounters;
+  }
 }
