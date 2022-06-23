871c167c08f2d4c6368fe818a4e42f4ad941f3eb
krisztian14testgit <krisztian14@citromail.hu>
2022-06-23 21:11:17 +0200

    material card and slider toggle were added

--- src/app/modules/statistic/statistic.module.ts
+++ src/app/modules/statistic/statistic.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 import { StatisticRoutingModule } from './statistic-routing.module';
 import { NgChartsModule } from 'ng2-charts';
@@ -16,7 +20,11 @@ import { TaskService } from '../task/services/task.service';
   imports: [
     CommonModule,
     StatisticRoutingModule,
-    NgChartsModule
+    FormsModule,
+    NgChartsModule,
+    /** Angular material */
+    MatCardModule,
+    MatSlideToggleModule
   ],
   providers: [
     TaskService
