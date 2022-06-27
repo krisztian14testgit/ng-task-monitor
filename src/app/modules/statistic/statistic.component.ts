2af6eb4e31df81f5fe6214ee5ff782863dcef656
krisztian14testgit <krisztian14@citromail.hu>
2022-06-27 10:00:49 +0200

    get report type form the navigated url

--- src/app/modules/statistic/statistic.component.ts
+++ src/app/modules/statistic/statistic.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { TaskService } from '../task/services/task.service';
 import { Task } from '../task/services/task.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-statistic',
@@ -11,28 +12,35 @@ import { Task } from '../task/services/task.model';
 export class StatisticComponent implements OnInit {
   /** Contains the task instances from the task Service. */
   public taskList: Task[] = [];
-  /** This a switcher, the tasks are created today or not. */
-  public isFilteredToday = true;
-  public toggleName: 'daily' | 'weekly';
+  /**
+   * This a switcher of the report. 
+   * If it is true showing daily report, otherwise weekly report.
+   */
+  public isDailyReport!: boolean;
   /** Contains the actual title of the diagramm. */
   public currentChartTitle!: string;
-  constructor(private readonly taskService: TaskService) {
+  constructor(private readonly taskService: TaskService,
+              private readonly router: Router) {
     this.currentChartTitle = 'Number of Task status';
-    this.toggleName = 'daily';
   }
 
   /** Gets all tasks from the service. */
   ngOnInit(): void {
+    this.getReportTypeFromUrl();
     this.getAllTasks();
-  }
-
-  public onChangeToggle(): void {
-    this.toggleName = this.isFilteredToday ? 'daily' : 'weekly';
   }
 
   private getAllTasks(): void {
     this.taskService.getAll()
     .subscribe((tasks: Task[]) => this.taskList = tasks);
+  }
+
+  /**
+   * Adjusts the isDailyReport switcher by the url path.
+   */
+  private getReportTypeFromUrl(): void {
+    const reportType = 'daily';
+    this.isDailyReport = this.router.url.includes(reportType);
   }
 
 }
