573236a2b0f61abbf453190401ff15239e03bd37
krisztian14testgit <krisztian14@citromail.hu>
2022-06-28 17:24:51 +0200

    chart.model got ChartLineRopert docs

--- src/app/modules/statistic/services/chart-date.model.ts
+++ src/app/modules/statistic/services/chart-date.model.ts
@@ -1,4 +1,9 @@
+/**
+ * The supported types of datasets of the line-chart which represents.
+ */
 export enum ChartLineReport {
+    /** Couting the completed tasks in week. */
     CompletedTask,
+    /** Amount of the spent timer of the completed Tasks. */
     SpentTimer
 }
