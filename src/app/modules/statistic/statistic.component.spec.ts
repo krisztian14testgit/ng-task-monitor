610babfc0e9ba2cf1e9258eeac190aeb70163ffc
krisztian14testgit <krisztian14@citromail.hu>
2022-06-29 15:56:05 +0200

    statistic tests are created

--- src/app/modules/statistic/statistic.component.spec.ts
+++ src/app/modules/statistic/statistic.component.spec.ts
@@ -1,14 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { MockTaskService } from 'src/app/tests/mock-services/mock-task.service';
+import { TaskService } from '../task/services/task.service';
 import { StatisticComponent } from './statistic.component';
 
 describe('StatisticComponent', () => {
   let component: StatisticComponent;
   let fixture: ComponentFixture<StatisticComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StatisticComponent ]
+      imports: [ RouterTestingModule.withRoutes([
+        { path: 'statistic/daily', component: StatisticComponent },
+        { path: 'statistic/weekly', component: StatisticComponent }
+      ])],
+      declarations: [ StatisticComponent ],
+      providers: [
+        { provide: TaskService, useClass: MockTaskService }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   });
@@ -16,10 +30,35 @@ describe('StatisticComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StatisticComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('should get all tasks', () => {
+    component.ngOnInit();
+    expect(component.taskList).toBeDefined();
+    expect(component.taskList.length).toBeGreaterThan(0);
+  });
+  
+  fit('should get report type from the url, isDailyReport to be true', fakeAsync(() => {
+    router.navigateByUrl('statistic/daily');
+    tick(100);
+    
+    component.ngOnInit();
+    expect(component.isDailyReport).toBeTrue();
+    expect(component.loadedReportCharts).toEqual(component['dailyReportCharts']);
+  }));
+
+  fit('should get report type from the url, isDailyReport to be false', fakeAsync(() => {
+    router.navigateByUrl('statistic/weekly');
+    tick(100);
+    
+    component.ngOnInit();
+    expect(component.isDailyReport).toBeFalse();
+    expect(component.loadedReportCharts).toEqual(component['weeklyReportCharts']);
+  }));
 });
