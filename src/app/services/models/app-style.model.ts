9bcb096979d07a62d2797aa94656a3716ef64746
krisztian14testgit <krisztian14@citromail.hu>
2022-07-11 17:14:45 +0200

    StyleThemes moved into app-style.model

---
+++ src/app/services/models/app-style.model.ts
@@ -0,0 +1,8 @@
+/** Represents the defined themes of the app. */
+export enum StyleThemes {
+    /** Default theme of the app. */
+    Light = '',
+    Dark = 'dark-theme',
+    /** Blue theme with dragon. */
+    BlueDragon = 'blue-dragon'
+}
