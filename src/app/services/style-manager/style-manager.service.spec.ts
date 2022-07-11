bfc36d440a4cd4739c6222e6ccd5e94d907564ad
krisztian14testgit <krisztian14@citromail.hu>
2022-07-11 15:10:46 +0200

    stlye-manager features, test cases done, works

--- src/app/services/style-manager/style-manager.service.spec.ts
+++ src/app/services/style-manager/style-manager.service.spec.ts
@@ -1,16 +1,108 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject} from '@angular/core/testing';
 
 import { StyleManagerService } from './style-manager.service';
+import { StyleThemes } from '../models/app-style.model';
 
 describe('StyleManagerService', () => {
   let service: StyleManagerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [StyleManagerService]
+    });
     service = TestBed.inject(StyleManagerService);
+    // default mat class
+    document.body.classList.add('mat-typography');
   });
 
-  it('should be created', () => {
+  it('should be created', inject([StyleManagerService], (service: StyleManagerService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should check the given theme Key is valid', () => {
+    // wrong values: null, undefined, '', 'other'
+    let wrongValue: null | undefined | string = null;
+    expect(service.isValidTheme(wrongValue as any)).toBeFalse();
+
+    wrongValue = undefined;
+    expect(service.isValidTheme(wrongValue as any)).toBeFalse();
+
+    wrongValue = '';
+    expect(service.isValidTheme(wrongValue)).toBeFalse();
+
+    wrongValue = 'something';
+    expect(service.isValidTheme(wrongValue)).toBeFalse();
+
+    // good values: StyleThemes Keys
+    const themeKeys = Object.keys(StyleThemes);
+    for (const key of themeKeys) {
+      expect(service.isValidTheme(key)).toBeTrue();
+    }
+  });
+
+  it('should NOT work isValidTheme checking on the values of StyleThemes', () => {
+    const themeValues = Object.values(StyleThemes);
+    for (const value of themeValues) {
+      expect(service.isValidTheme(value)).toBeFalse();
+    }
+  });
+
+  it('should get theme form the body classList', () => {
+    let themeValue = service['getThemeFromBodyTag']();
+    let expectedValue = '';
+    expect(themeValue).toBe(expectedValue);
+    expect(document.body.classList.length).toBe(1);
+
+    service['insertTheme'](StyleThemes.BlueDragon);
+    themeValue = service['getThemeFromBodyTag']();
+    expectedValue = StyleThemes.BlueDragon;
+    expect(themeValue).toBe(expectedValue);
+    expect(document.body.classList.length).toBe(2);
+
+    service['insertTheme'](StyleThemes.Dark);
+    themeValue = service['getThemeFromBodyTag']();
+    expectedValue = StyleThemes.Dark;
+    expect(themeValue).toBe(expectedValue);
+    expect(document.body.classList.length).toBe(3);
+    document.body.classList.remove('mat-typography', StyleThemes.Dark, StyleThemes.BlueDragon);
+  });
+
+  it('should get empty value if the body classList is empty', () => {
+    document.body.classList.remove('mat-typography', StyleThemes.Dark, StyleThemes.BlueDragon);
+    const themeValue = service['getThemeFromBodyTag']();
+    const expectedValue = '';
+    expect(themeValue).toBe(expectedValue);
+  });
+
+  it('should turn on the selected theme and the previous one is removed', () => {
+    // Default classList length=1, as one class definition is included.
+    expect(document.body.classList.length).toBe(1);
+
+    // add wrong values, not added into the classList of the body tag.
+    let wrongValue: null | string = null;
+    service.turnOnTheme(wrongValue as any);
+    expect(document.body.classList.length).toBe(1);
+
+    wrongValue = '';
+    service.turnOnTheme(wrongValue as any);
+    expect(document.body.classList.length).toBe(1);
+
+    wrongValue = 'wrong';
+    service.turnOnTheme(wrongValue as any);
+    expect(document.body.classList.length).toBe(1);
+
+    // add good value from StyleThemes
+    let theme = StyleThemes.Dark;
+    service.turnOnTheme(theme);
+    expect(document.body.classList.length).toBe(2);
+
+    theme = StyleThemes.BlueDragon;
+    service.turnOnTheme(theme);
+    expect(document.body.classList.length).toBe(2);
+
+    // Light theme wont be added into classList, it is default, its value is empty.
+    theme = StyleThemes.Light;
+    service.turnOnTheme(theme);
+    expect(document.body.classList.length).toBe(1);
   });
 });
--- src/app/services/style-manager/style-manager.service.ts
+++ src/app/services/style-manager/style-manager.service.ts
@@ -1,54 +1,80 @@
 import { Injectable } from '@angular/core';
+import { StyleThemes } from '../models/app-style.model';
 
-enum StyleThemes {
-  /** Default theme of the app. */
-  Light = '',
-  Dark = 'dark-theme',
-  BlueDragon = 'blue-dragon'
-}
-
+/**
+ * Handler the attaching and removing the style themes of the App.
+ */
 @Injectable()
 export class StyleManagerService {
-  private classTheme: string;
 
-  constructor() {
-    this.classTheme = this.getThemeFromBodyTag();
+  /**
+   * Returns true if the given themeKey is belong StyleThemes enum,
+   * oterwise returns false in any values.
+   * 
+   * @param themeKey The key values from the StyleThemes
+   * @returns boolean
+   */
+  public isValidTheme(themeKey: string): boolean {
+    const keys = Object.keys(StyleThemes);
+    return keys.includes(themeKey);
   }
 
-  //#region Properties
-  public get isDefaultTheme(): boolean {
-    return this.classTheme === StyleThemes.Light;
-  }
-  public get isDarkTheme(): boolean {
-    return this.classTheme === StyleThemes.Dark;
-  }
-
-  public get isBlueDragonTheme(): boolean {
-    return this.classTheme === StyleThemes.BlueDragon;
-  }
-  //#endregion
-
+  /**
+   * Activates the given themes, the previous one will be removed from
+   * the body classList.
+   * @param theme The value of StyleThemes
+   */
   public turnOnTheme(theme: StyleThemes): void {
     const prevTheme = this.getThemeFromBodyTag();
-    if (this.removeTheme(prevTheme)) {
+    if (prevTheme) {
+      this.removeTheme(prevTheme);
+    }
+
+    const themeValues = Object.values(StyleThemes);
+    if (themeValues.includes(theme)) {
       this.insertTheme(theme);
     }
   }
 
-  private getThemeFromBodyTag(): StyleThemes {
-    if (document.body) {
-      const lastIndex = document.body.classList.length;
-      const classTheme = document.body.classList[lastIndex];
-      return classTheme as StyleThemes;
-    }
+  /**
+   * Returns the last index of the last item from the classList of the body.
+   * If the classList contains one class defition or it is empty, return -1;
+   * @returns index
+   */
+  private getLastIndexOfBodyClass(): number {
+    // occupied value: mat-typography, zero index is occupied
+    const lastIndex = document.body.classList.length - 1;
+    return  lastIndex > 0 ? lastIndex : -1; 
 
-    return StyleThemes.Light;
   }
 
-  private removeTheme(theme: StyleThemes): boolean {
+  /**
+   * Returns the last class themes form the classList of the body.
+   * @returns themes value
+   */
+  private getThemeFromBodyTag(): string {
     if (document.body) {
-      if (document.body.classList.contains(theme)) {
-        document.body.classList.remove(theme);
+      const lastIndex = this.getLastIndexOfBodyClass();
+      if (lastIndex > -1) {
+        return document.body.classList[lastIndex];
+      }
+    }
+
+    // Return default: StyleThemes.Light
+    return '';
+  }
+
+  /**
+   * Removes the given style theme from the classList of the body.
+   * 
+   * Returns true if the removing is success, otherwise returns false.
+   * @param themeValue The value of the StyleThemes.
+   * @returns boolean
+   */
+  private removeTheme(themeValue: string): boolean {
+    if (document.body) {
+      if (document.body.classList.contains(themeValue)) {
+        document.body.classList.remove(themeValue);
         return true;
       }
     }
@@ -56,8 +82,12 @@ export class StyleManagerService {
     return false;
   }
 
+  /**
+   * Adds the given style theme into the classList of the body.
+   * @param theme The value of the StyleThemes.
+   */
   private insertTheme(theme: StyleThemes): void {
-    if (document.body) {
+    if (theme && document.body) {
       document.body.classList.add(theme);
     }
   }
