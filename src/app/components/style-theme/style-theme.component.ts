b09ebc0b64af3c712bea7d52998d36c9a5da5df4
krisztian14testgit <krisztian14@citromail.hu>
2022-07-12 09:09:44 +0200

    style-theme comp missing docs was added

--- src/app/components/style-theme/style-theme.component.ts
+++ src/app/components/style-theme/style-theme.component.ts
@@ -1,47 +1,73 @@
 import { Component } from '@angular/core';
 import { MatRadioChange } from '@angular/material/radio';
-import { StyleManagerService, StyleThemes } from '../../services/style-manager/style-manager.service';
+import { StyleThemes } from 'src/app/services/models/app-style.model';
 
+import { StyleManagerService } from '../../services/style-manager/style-manager.service';
+
+/**
+ * Changes the style themes of the app with the radio button items.
+ */
 @Component({
   selector: 'app-style-theme',
   templateUrl: './style-theme.component.html',
   styleUrls: ['./style-theme.component.css']
 })
 export class StyleThemeComponent {
+  /** Contains the key names of the StyleThemes enum. */
   public styleThemes: string[];
+  /** Stores the selected theme's key name. */
   public selectedTheme: string;
-  private readonly storageKey = 'savedTheme';
+  private readonly STORAGE_KEY = 'savedTheme';
+  /** Default theme is StyleThemes.Light, stored name: 'Light'. */
   private readonly defaultTheme: string;
 
   constructor(private readonly styleManagerService: StyleManagerService) {
     this.styleThemes = Object.keys(StyleThemes);
     this.defaultTheme = this.styleThemes[0];
-    this.selectedTheme = this.getStoredTheme();
+    this.selectedTheme = this.getStoredThemeFromLocalStorage();
     
-    // TODO: default light selection not work at radio button
     if (this.selectedTheme) {
       this.changeTheme(this.selectedTheme);
     }
   }
 
-  public onChangeTheme(event: MatRadioChange): void {
-    this.selectedTheme = event.value;
-    if (this.selectedTheme && this.styleManagerService.isValidTheme(this.selectedTheme)) {
+  /**
+   * This is a change event functon.
+   * 
+   * It triggers when radio button item is selected.
+   * Changes the style theme of the App.
+   * @event change
+   * @param event MatRadio event.
+   */
+  public onChangeTheme($event: MatRadioChange): void {
+    this.selectedTheme = $event.value;
+    if (this.selectedTheme) {
       this.changeTheme(this.selectedTheme);
       console.log('body class=', document.body.className);
     }
   }
 
-  private getStoredTheme(): string {
-    const theme = localStorage.getItem(this.storageKey);
+  /**
+   * Returns the stored style theme of the App which was applied.
+   * @returns StyleThemes value.
+   */
+  private getStoredThemeFromLocalStorage(): string {
+    const theme = localStorage.getItem(this.STORAGE_KEY);
     return theme ? theme : this.defaultTheme;
   }
 
+  /**
+   * Changes the style theme of the app with style manger service and
+   * stores the selected theme into the local storage.
+   * @param themeKey The key of the StyleThemes.
+   */
   private changeTheme(themeKey: string): void {
-    // convert string to enum: StyleThemes['enumString']
-    const themeValue = StyleThemes[themeKey as keyof typeof StyleThemes];
-    this.styleManagerService.turnOnTheme(themeValue);
-    // saves it localStorage
-    localStorage.setItem(this.storageKey, this.selectedTheme);
+    if (this.styleManagerService.isValidTheme(themeKey)) {
+      // convert string to enum: StyleThemes['enumString']
+      const themeValue = StyleThemes[themeKey as keyof typeof StyleThemes];
+      this.styleManagerService.turnOnTheme(themeValue);
+      // saves it localStorage
+      localStorage.setItem(this.STORAGE_KEY, themeKey);
+    }
   }
 }
