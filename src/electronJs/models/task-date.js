6585411e5fa47b566ee11f03e2e7a1d7974d812f
krisztian14testgit <krisztian14@citromail.hu>
2022-08-28 15:38:17 +0200

    effective performance creationDate checking, run faster

--- src/electronJs/models/task-date.js
+++ src/electronJs/models/task-date.js
@@ -1,7 +1,13 @@
 "use strict";
 /* eslint-disable no-undef */
 
+/** diff7days milliSec = 7-days * 24-hours * 60-min * 60-sec * 1000-millisec */
+const DIFF_7DAYS_MILLISEC  = 7 * 24 * 60 * 60 * 1000;
+
 class TaskDate {
+
+    /** Contains the current system date of pc. It will be adjusted/reset when callig removeOldTaskByDate method. */
+    static _currentSystemDate = undefined;
     /**
      * Returns the new array which won't contain those tasks which are created more than 7 days/one week.
      * Removing those tasks which createdDate is more older then a week.
@@ -12,7 +18,8 @@ class TaskDate {
     static removeOldTaskByDate(taskCreationDateProp= '', taskList = []) {
         // deep copy items by spread operators
         let retArray = [];
-        
+        TaskDate._currentSystemDate = new Date();
+
         if (taskCreationDateProp && taskList.length > 0) {
             retArray = [...taskList];
             for (let index = retArray.length -1; index > -1; index--) {
@@ -36,11 +43,8 @@ class TaskDate {
      */
     static _isDateOlderThen7days(creationDate = '') {
         if (creationDate) {
-            const currentSystemDate = new Date();
             const givenDate = new Date(creationDate);
-            // diff7days milliSec = days * hours * min * sec * millisec
-            const diff7days_milliSec = 7 * 24 * 60 * 60 * 1000;
-            return currentSystemDate.getTime() - givenDate.getTime() >= diff7days_milliSec;
+            return TaskDate._currentSystemDate.getTime() - givenDate.getTime() >= DIFF_7DAYS_MILLISEC;
         }
 
         return false;
