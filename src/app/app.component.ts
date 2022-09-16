b430222976840d809eaf9755de6b2a17fecdfd34
krisztian14testgit <krisztian14@citromail.hu>
2022-09-16 12:10:25 +0200

    show message web API is not implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { AlertType } from './components/alert-window/alert.model';
+import { AlertMessageService } from './services/alert-message/alert-message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements AfterViewInit {
+  constructor(private readonly alertService: AlertMessageService) {}
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    const noAPI = `This app hasn't been linked with web API yet!`;
+    this.alertService.sendMessage(noAPI, AlertType.Info);
+  }
 }
