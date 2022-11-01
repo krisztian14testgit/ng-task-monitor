7372ca85d1f82349f713352fd5fb4764eda41cc4
krisztian14testgit <krisztian14@citromail.hu>
2022-11-01 20:35:09 +0100

    router test zone issues were fixed, test run well

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -3,9 +3,9 @@ import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core
 import { Router } from '@angular/router';
 import { Location } from "@angular/common";
 import { RouterTestingModule } from '@angular/router/testing';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 import { HeaderComponent } from './header.component';
+import { NavigationExtras } from '@angular/router';
 
 @Component({})
 class FakedRouteComponent {}
@@ -34,8 +34,7 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule.withRoutes(routeTable),
-        MatGridListModule
+        RouterTestingModule.withRoutes(routeTable)
       ],
       declarations: [ HeaderComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -64,12 +63,16 @@ describe('HeaderComponent', () => {
   });
 
   it('should navigate to faked home site', fakeAsync(()=> {
-    router.navigate(['']); // navige to tasks/inprogress
+    // navige to home site
+    spyOn(router, 'navigate').and.callThrough();
+    router.navigate(['']);
+    fixture.autoDetectChanges();
     tick(1);
     expect(location.path()).toBe('/home');
   }));
 
   it('should get titleOfRoute from the router event', fakeAsync(() => {
+    spyOn(router, 'navigate').and.callThrough();
     // default rediretion '/tasks/all' in app-routing.module.ts
     const defaultRedirectionTitle = 'All tasks';
     expect(component.titleOfRoute).toBe(defaultRedirectionTitle);
@@ -80,6 +83,7 @@ describe('HeaderComponent', () => {
     const navigationPaths = ['tasks/inprogress', 'weekly'];
     for (const navUrl of navigationPaths) {
       router.navigate([ navUrl ]);
+      fixture.autoDetectChanges();
       tick(1);
       expect(location.path()).toBe(`/${navUrl}`);
       // routerDict contains the title of each router path.
