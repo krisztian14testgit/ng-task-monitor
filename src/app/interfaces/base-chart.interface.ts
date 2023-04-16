a75182f77d3189c53987c4dca53791f5b62ca474
krisztian14testgit <krisztian14.s@outlook.com>
2023-04-16 23:21:06 +0200

    member of assigned for IBaseChart

--- src/app/interfaces/base-chart.interface.ts
+++ src/app/interfaces/base-chart.interface.ts
@@ -1,21 +1,34 @@
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { Task } from '../modules/task/services/task.model';
 
-/** Contains the pre-defined properties and methods for the different Chart.components. */
+/** It contains the pre-defined properties and methods for the different Chart.components. */
 export interface IBaseChart {
-    /** The chartjs types: line, pie, bar, ... */
+    /**
+     * The chartjs types: line, pie, bar, ...
+     * @memberof IBaseChart
+     */
     readonly currentChartType: ChartType;
-    /** The chartjs options settings. */
+    /**
+     * The chartjs options settings.
+     * @memberof IBaseChart
+     */
     readonly currentChartOption: ChartConfiguration['options'];
-    /** The chartjs Data structure. */
+    /**
+     * The chartjs Data structure.
+     * @memberof IBaseChart
+     */
     currentChartDate: ChartData<'pie' | 'line', number[], string | string[]>;
-    /** The chartjs plugins setting. */
+    /**
+     * The chartjs plugins setting.
+     * @memberof IBaseChart
+     */
     readonly currentChartPlugins: never[];
     /** 
      * The property is optional. Default: undefined.
      * Stores the actual chart titles which will be shown on the chart.
      * 
      * @private privilege level
+     * @memberof IBaseChart
      */
     readonly _chartLabels?: string[];
     /**
@@ -23,6 +36,7 @@ export interface IBaseChart {
      * Stores the callback funtion references which calculate the completed Task or spent time data.
      * 
      * @private privilege level
+     * @memberof IBaseChart
      */
     readonly _chartDataCallBack?: ((list: any[]) => number[])[];
     /**
@@ -30,6 +44,8 @@ export interface IBaseChart {
      * Settings the label and datasets of the line chart.
      * 
      * @param taskList The elements of the taskList.
+     * @private privilege level
+     * @memberof IBaseChart
      */
-    setCurrentChartDataBy(taskList: Task[]): void;
+    _setCurrentChartDataBy(taskList: Task[]): void;
 }
