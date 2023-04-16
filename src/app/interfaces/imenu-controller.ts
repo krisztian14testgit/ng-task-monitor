fef8ca4c4931066174c79226057f7c90c9bab4f2
krisztian14testgit <krisztian14.s@outlook.com>
2023-04-16 22:47:12 +0200

    MenuControllar changed to interface struct

--- src/app/interfaces/imenu-controller.ts
+++ src/app/interfaces/imenu-controller.ts
@@ -3,11 +3,11 @@
  * Contains the basic method for the menu-item controller which have to be implemented.
  * 
  */
-export class IMenuController {
+export interface IMenuController {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     /** Displays the sub-menu items. */
-    show() {}
+    show(): void;
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     /** Hides the sub-menu items */
-    close() {}
+    close(): void;
 }
