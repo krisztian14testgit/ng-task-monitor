16e4ac57ecdb342080de069aa4eaa8bfab54018e
krisztian14testgit <krisztian14.s@outlook.com>
2023-05-11 20:02:06 +0200

    devided the ngOnChanges code what they do exactly.

--- src/app/components/menu-item/menu-item.component.ts
+++ src/app/components/menu-item/menu-item.component.ts
@@ -30,20 +30,45 @@ export class MenuItemComponent implements OnChanges {
    */
   ngOnChanges(changes: SimpleChanges): void {
     if (this.menuItems_dict && this.menuItems_dict === changes.menuItems_dict.currentValue) {
-      this.menuLabelKeys = Object.keys(this.menuItems_dict);
-      this.isDisplayedLabels = this.menuLabelKeys.length > 0;
+      this.setMenuItemsWithLables();
     }
 
     if (this.menuItems_dict && !this.isDisplayedLabels) {
-      // get menu items into one array: [ [a,b], [c,d] ] => [a,b,c,d]
-      const sublistInList = Object.values(this.menuItems_dict);
-      for (const getList of sublistInList) {
-        this.menuItemValues = [...this.menuItemValues, ...getList];
-      }
+      this.setMenuItemsWithoutLabelTag();
       // reset menu labels
       this.menuLabelKeys = [];
     }
     
+    this.setMenuLabels();
+  }
+
+  /**
+   * Collects the label keys from `the this.menuItems_dict` and 
+   * displaying complex menu(group label with menu items) by the `the this.menuItems_dict`
+   * with its own labelKeys
+   */
+  private setMenuItemsWithLables(): void {
+    this.menuLabelKeys = Object.keys(this.menuItems_dict);
+    this.isDisplayedLabels = this.menuLabelKeys.length > 0;
+  }
+
+  /**
+   * Fills in the menuItemValues from menuItemsWithLabel without lables tag.
+   * @description
+   * menu-container:
+   * * menu-item1
+   * * menu-item2
+   * * menu-item3
+   */
+  private setMenuItemsWithoutLabelTag(): void {
+    // get menu items into one array: [ [a,b], [c,d] ] => [a,b,c,d]
+    const sublistInList = Object.values(this.menuItems_dict);
+    for (const getList of sublistInList) {
+      this.menuItemValues = [...this.menuItemValues, ...getList];
+    }
+  }
+
+  private setMenuLabels(): void {
     if (this.menuItems_dict && this.isDisplayedLabels) {
       // displayed true to collect labelKeys again.
       this.menuLabelKeys = Object.keys(this.menuItems_dict);
